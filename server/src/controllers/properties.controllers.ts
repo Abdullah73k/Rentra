0b8c65d0579d66fa3b7f262d97cde493794d87b3
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-10-12 16:31:25 -0400

    PM-8[PropertyAPI][Task]: changed userProperty controller name to include req method for better clarity

--- server/src/controllers/properties.controllers.ts
+++ server/src/controllers/properties.controllers.ts
@@ -43,7 +43,7 @@ export const getUserPropertyData = (
 	}
 };
 
-export const userProperties = (
+export const getUserProperties = (
 	req: Request<{ userId: string }>,
 	res: Response
 ) => {
--- server/src/routes/properties.routes.ts
+++ server/src/routes/properties.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getUserPropertyData } from "../controllers/properties.controllers.js";
-import { userProperties } from "../controllers/properties.controllers.js";
+import { getUserProperties } from "../controllers/properties.controllers.js";
 
 const router: Router = Router();
 
-router.get("/:userId", userProperties);
+router.get("/:userId", getUserProperties);
 router.get("/:propertyId", getUserPropertyData);
 
 export default router;
