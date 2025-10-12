d7d09fd35f037c9572cc53387323013b3daeedf7
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-10-12 14:51:33 -0400

    PM-8[PropertyAPI][Task]: created API endpoint to get user property data

--- server/src/controllers/properties.controllers.ts
+++ server/src/controllers/properties.controllers.ts
@@ -1,1 +1,44 @@
-import { Router } from "express";
+import type { Request, Response } from "express";
+import { StatusCodes } from "../constants/statusCodes.js";
+import { validateUUID } from "../utils/validation.utils.js";
+
+export const getUserPropertyData = (
+	req: Request<{ propertyId: string }, {}, {}, {}>,
+	res: Response
+) => {
+	try {
+		// TODO: Must add auth validation once we integrate BetterAuth
+		// 1. Validate propertyId as valid UUID
+		const { propertyId } = req.params;
+
+		const result = validateUUID(propertyId);
+
+		if (!result.success) {
+			return res.status(StatusCodes.BAD_REQUEST).json({
+				error: true,
+				message: "Property ID should be a valid UUID",
+			});
+		}
+		// 2. Query DB to get all property info
+
+		// 3. Send response based on property info stored in DB
+
+		return res.status(StatusCodes.SUCCESS).json({
+			error: false,
+			message: "Successfully fetched user property and all associated info",
+			data: {
+				property: {},
+				propertyInfo: {},
+				loan: {},
+				tenant: {},
+				lease: {},
+				transaction: {},
+			},
+		});
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+			error: true,
+			message: "Internal server error, could not fetch property info",
+		});
+	}
+};
--- server/src/routes/properties.routes.ts
+++ server/src/routes/properties.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userProperties } from "../controllers/auth.controllers.js";
+import { getUserPropertyData } from "../controllers/properties.controllers.js";
 
 const router: Router = Router()
 
 router.get("/:userId", userProperties)
+router.get("/:propertyId", getUserPropertyData)
 
 export default router
