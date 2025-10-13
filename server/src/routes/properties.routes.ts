8799fd9125d00d52e2a640a0d4be0d732cc11f60
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-10-13 12:25:09 -0400

    PM-12[PropertyAPI][Task]: created delete property api endpoint

--- server/src/routes/properties.routes.ts
+++ server/src/routes/properties.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getUserPropertyData } from "../controllers/properties.controllers.js";
+import {
+	deleteUserProperty,
+	getUserPropertyData,
+} from "../controllers/properties.controllers.js";
 import { getUserProperties } from "../controllers/properties.controllers.js";
 
 const router: Router = Router();
 
 router.get("/:userId", getUserProperties);
 router.get("/:propertyId", getUserPropertyData);
+router.delete("/:propertyId", deleteUserProperty);
 
 export default router;
