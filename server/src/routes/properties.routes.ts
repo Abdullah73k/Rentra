140d70cd53069866b9a4bce4a2a928596a48e8cf
Ahmad Khamis <ahmadkhamis008@gmail.com>
2025-10-14 17:24:42 +0400

    PM-11 [PropertyAPI][task]: put error handling logic in validatePropertyInfo function and changed the name of create new property route

--- server/src/routes/properties.routes.ts
+++ server/src/routes/properties.routes.ts
@@ -7,6 +7,6 @@ router.get("/:userId", getUserProperties);
 router.get("/:propertyId", getUserPropertyData);
 router.delete("/:propertyId", deleteUserProperty);
 
-router.post("/", postUserPropertyData)
+router.post("/create", postUserPropertyData)
 
 export default router;
--- server/src/utils/validation.utils.ts
+++ server/src/utils/validation.utils.ts
@@ -1,5 +1,6 @@
-import { z } from "zod"
+import { success, z, type ZodIssue } from "zod"
 import { propertyInfoValidationSchema } from "../schemas/propertyInfo.schemas.js"
+import { da } from "zod/locales"
 
 export type PropertyInfo = z.infer<typeof propertyInfoValidationSchema>
 
@@ -10,5 +11,17 @@ export function validateUUID(userId: string) {
 }
 export function validatePropertyInfo(data: PropertyInfo) {
     const result = propertyInfoValidationSchema.safeParse(data)
-    return result
+    if (!result.success) {
+        const formatted = result.error.issues.map((issue: ZodIssue) => ({
+            feild: issue.path.join("."),  // e.g. "property.userId"
+            message: issue.message,      // e.g. "Invalid UUID"
+        }));
+        const errorObj: {success: false, errors: typeof formatted} = {success: false, errors: formatted}
+        return errorObj
+    }
+    const validatedData: {success: true, data: PropertyInfo} = {
+        success: true,
+        data: result.data
+    }
+    return validatedData
 }
