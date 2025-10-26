d60b4f1087c76f4a007e81e353cb2b2d89811f0b
Ahmad Khamis <ahmadkhamis008@gmail.com>
2025-10-26 20:19:47 +0400

    PM-23 [PropertyAPI][task]: added updated user table  migration

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,27 +1,66 @@
 import { betterAuth } from "better-auth";
 import { pool } from "../config/pg.config.js";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "../constants/auth.constants.js";
 
 export const auth = betterAuth({
 	database: pool,
+	advanced: {
+		database: {
+			generateId: () => crypto.randomUUID()
+		}
+	},
+	user: {
+		additionalFields: {
+			country: {
+				type: "string",
+				required: true,
+				input: true
+			},
+			currency: {
+				type: "string",
+				required: true,
+				input: true
+			},
+			vatProfile: {
+				type: "number",
+				required: true,
+				input: true
+			}
+		}
+	},
 	emailAndPassword: {
 		enabled: true,
 		requireEmailVerification: true,
-		sendResetPassword: async ({user, url, token}, request) => {
-    //   await sendEmail({
-    //     to: user.email,
-    //     subject: "Reset your password",
-    //     text: `Click the link to reset your password: ${url}`,
-    //   });
-	// logic for sending email
-    },
-    onPasswordReset: async ({ user }, request) => {
-      // your logic here
-      console.log(`Password for user ${user.email} has been reset.`);
-    },
+		sendResetPassword: async ({ user, url, token }, request) => {
+			//   await sendEmail({
+			//     to: user.email,
+			//     subject: "Reset your password",
+			//     text: `Click the link to reset your password: ${url}`,
+			//   });
+			// logic for sending email
+		},
+		onPasswordReset: async ({ user }, request) => {
+			// your logic here
+			console.log(`Password for user ${user.email} has been reset.`);
+		},
 	},
 	emailVerification: {
-		sendVerificationEmail: async ({user, url, token}, request) => {
+		sendVerificationEmail: async ({ user, url, token }, request) => {
 			// send the email with following properties
+		}
+	},
+	socialProviders: {
+		google: {
+			clientId: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET
+		},
+		discord: {
+			clientId: DISCORD_CLIENT_ID,
+			clientSecret: DISCORD_CLIENT_SECRET
+		},
+		github: {
+			clientId: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_CLIENT_SECRET
 		}
 	}
 });
