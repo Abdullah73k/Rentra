12ea319c69752654725a750eca8d7681a4c3bd8e
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-11-04 10:04:20 -0500

    PM-25[PropertyAPI][Task]: updated schema structure

---
+++ server/src/schemas/patch.schemas.ts
@@ -0,0 +1,43 @@
+import z from "zod";
+import * as POST from "./post.schemas.js";
+import { ReusableTypes } from "./reusable.schemas.js";
+
+export const propertySchema = POST.propertySchema.extend({
+	id: ReusableTypes.uuid,
+});
+
+export const propertyInfoSchema = POST.propertyInfoSchema.extend({
+	id: ReusableTypes.uuid,
+	propertyId: ReusableTypes.uuid,
+});
+
+export const documentSchema = POST.documentSchema.extend({
+	id: ReusableTypes.uuid,
+});
+
+const loanSchema = POST.loanSchema.extend({
+	id: ReusableTypes.uuid,
+	propertyId: ReusableTypes.uuid,
+});
+
+const tenantSchema = POST.tenantSchema.extend({
+	id: ReusableTypes.uuid,
+});
+
+const leaseSchema = POST.leaseSchema.extend({
+	id: ReusableTypes.uuid,
+});
+
+export const transactionSchema = POST.transactionSchema.extend({
+	id: ReusableTypes.uuid,
+});
+
+export const propertyDataSchema = z
+	.object({
+		property: propertySchema,
+		propertyInfo: propertyInfoSchema,
+		loan: loanSchema,
+		tenant: tenantSchema,
+		lease: leaseSchema,
+	})
+	.partial();
