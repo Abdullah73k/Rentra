7d030ac02aba4ad5936a955fe6f356119a5cee30
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-11-14 16:29:26 -0500

    PM-39[PropertyAPI][Task]: added create tx service

--- server/src/services/transaction.services.ts
+++ server/src/services/transaction.services.ts
@@ -1,8 +1,13 @@
 import { TransactionRepository } from "../repositories/transaction.repositories.js";
+import * as DB from "../types/db.types.js";
 
 export const TransactionService = {
+	async create(transaction: DB.CreateTransaction) {
+		const result = await TransactionRepository.createTransaction(transaction);
+		return result;
+	},
 	async delete(transactionId: string) {
 		const result = await TransactionRepository.delete(transactionId);
-        return result
+		return result;
 	},
 };
