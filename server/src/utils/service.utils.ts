b537a5ef73233387997f90ebb7a08b4ada64d7ad
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-11-23 21:39:28 -0500

    PM-40[PropertyAPI][Task]: added validation error if for query in tx since i added input validation checks before query

--- server/src/utils/service.utils.ts
+++ server/src/utils/service.utils.ts
@@ -1,5 +1,6 @@
 import { pool } from "../configs/pg.config.js";
 import { DBError } from "../errors/db.errors.js";
+import { ValidationError } from "../errors/validation.errors.js";
 
 export async function queryInTransaction<T, U>(
 	queryFn: (data: T) => Promise<U>,
@@ -16,10 +17,14 @@ export async function queryInTransaction<T, U>(
 		return query;
 	} catch (error) {
 		await client.query("ROLLBACK");
-		console.log("DB transaction error: ", error);
+		console.log("DB transaction unknown error: ", error);
 		if (error instanceof DBError) {
 			console.error(error.message, error);
 			throw new DBError(error.statusCode, errMsg, error);
+		}
+		if (error instanceof ValidationError) {
+			console.error(error.message, error);
+			throw new ValidationError(error.message);
 		}
 	} finally {
 		client.release();
