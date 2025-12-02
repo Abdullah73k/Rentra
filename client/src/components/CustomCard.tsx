c7631adcbd931ef59296546a5cb52e57aa17ccf0
Ahmad Khamis <ahmadkhamis008@gmail.com>
2025-12-02 11:38:00 +0400

    PM-52 [FrontendAuth][task]: made custom card and used it where needed

---
+++ client/src/components/CustomCard.tsx
@@ -0,0 +1,31 @@
+import type React from "react";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { type LucideProps } from "lucide-react";
+
+type CardProps = {
+  children: React.ReactNode;
+  Icon?: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+  title: string;
+};
+
+const CustomCard = ({ children, Icon, title }: CardProps) => {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="flex items-center gap-2">
+          {Icon && <Icon className="h-5 w-5" />}
+          {title}
+        </CardTitle>
+        <Button variant="outline" size="sm" className="gap-1 bg-transparent">
+          Edit
+        </Button>
+      </CardHeader>
+      <CardContent className="space-y-2">{children}</CardContent>
+    </Card>
+  );
+};
+
+export default CustomCard;
