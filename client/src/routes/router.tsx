95163362fbe35a5e4c592379f9ba352769201982
Ahmad Khamis <ahmadkhamis008@gmail.com>
2025-12-07 23:19:36 +0400

    PM-58 [FrontendAuth][task]: added forgot password functionality

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -6,6 +6,8 @@ import UserSettings from "../pages/auth/UserSettings";
 import Home from "../pages/Home";
 import Property from "../pages/properties/Property";
 import PropertiesDashboard from "../pages/properties/PropertiesDashboard";
+import ForgotPassword from "@/pages/auth/ForgotPassword";
+import ResetPassword from "@/pages/auth/ResetPassword";
 
 const auth = {
     path: "/auth",
@@ -23,6 +25,14 @@ const auth = {
         {    // protected
             path: "settings",
             element: <UserSettings /> 
+        },
+        {
+            path: "forgot-password",
+            element: <ForgotPassword />
+        },
+        {
+            path: "reset-password",
+            element: <ResetPassword />
         }
     ]
 }
