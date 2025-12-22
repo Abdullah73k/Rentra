0bb6aab3dd9962c79daaa843c0ec1789bfb97392
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-12-22 16:35:37 +0400

    PM-81[ServerTests][Task]: created tests for property endpoints, will continue them on PM-80

--- server/src/tests/routes/property-endpoints.test.ts
+++ server/src/tests/routes/property-endpoints.test.ts
@@ -3,14 +3,26 @@ import request from "supertest";
 import app from "../../app.js";
 import { StatusCodes } from "../../constants/statusCodes.constants.js";
 
-describe("Property API Endpoints", () => {
-	describe("GET /api/ping ", () => {
-		it(`Success should return 200 status code and '{ message: "pong" }'`, async () => {
+const userId = "d8ef0ed9-451e-4c81-a043-bd0495e67c2e";
 
-			const res = await request(app).get("/api/ping");
+describe("Property API Endpoints", () => {
+	describe("GET /api/properties/all/:userId ", () => {
+		it(`Success should return 200 status code and return all user properties`, async () => {
+			const res = await request(app).get(`/api/properties/all/${userId}`);
 
 			expect(res.statusCode).toBe(StatusCodes.SUCCESS);
-			expect(res.body.message).toStrictEqual("pong");
 		});
+
+		it(`Success should return 400 status code and return an validation error for invalid UUID user id`, async () => {
+			const res = await request(app).get(`/api/properties/all/invalid-user-id`);
+
+			expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+		});
+
+		// it(`Success should return 400 status code and return an validation error for invalid UUID user id`, async () => {
+		// 	const res = await request(app).get(`/api/properties/all/invalid-user-id`);
+
+		// 	expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+		// });
 	});
 });
