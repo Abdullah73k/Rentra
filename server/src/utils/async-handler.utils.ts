ee0783d4e53131bff4693d0b5c9c9d0f1eff8297
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-12-25 10:18:36 +0400

    PM-99[PropertyAPI][Task]: updated async handler to support generic for all req params

--- server/src/utils/async-handler.utils.ts
+++ server/src/utils/async-handler.utils.ts
@@ -1,6 +1,8 @@
 import type { RequestHandler } from "express";
 
-export const asyncHandler = <T>(fn: RequestHandler<T>): RequestHandler<T> => {
+export const asyncHandler = <T, U, K, B>(
+	fn: RequestHandler<T, U, K, B>
+): RequestHandler<T, U, K, B> => {
 	return (req, res, next) => {
 		Promise.resolve(fn(req, res, next)).catch(next);
 	};
