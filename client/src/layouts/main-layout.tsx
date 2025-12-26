91983d09bf5c67f8fc9096e763b0a501726ea285
Ahmad <ahmadkhamis008@gmail.com>
2025-12-26 09:58:09 +0400

    PM-93[FrontendUI][task]: added user icon to bottom left and add functionality to the buttons

--- client/src/layouts/main-layout.tsx
+++ client/src/layouts/main-layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import HeaderNavigation from "../components/header-navigation";
 import { Toaster } from "sonner";
 import AuthProvider from "@/providers/auth.provider";
+import UserIcon from "@/components/user-icon";
 
 const MainLayout = () => {
   return (
     <AuthProvider>
       <HeaderNavigation />
+      <UserIcon />
       <main>
         <Outlet />
         <Toaster />
