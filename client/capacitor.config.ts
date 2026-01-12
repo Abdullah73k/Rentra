cd843469d3548935a4e0665a6d5a510781cd39d4
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-01-12 11:03:16 -0500

    added capacitor config file

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	appId: "com.abdullah.propertymanagement",
+	appName: "property-management",
+	webDir: "dist",
+};
+
+export default config;
