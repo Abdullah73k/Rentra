c0abf222076d978647e457bab6f240ae0c9d652a
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-01-12 11:47:56 -0500

    PM-96[FrontendProperties][Task]: made get doc query more reusable

--- server/src/utils/repository.utils.ts
+++ server/src/utils/repository.utils.ts
@@ -83,19 +83,37 @@ export const getRowsFromTableWithId = {
 		propertyId,
 		documentId,
 	}: { client: PoolClient | undefined } & (
-		| { propertyId: string; documentId?: never }
+		| { propertyId: string | string[]; documentId?: never }
 		| { propertyId?: never; documentId: string }
 	)) {
 		const pool = dbConnection(client);
-		return await pool
-			.select()
-			.from(documents)
-			.where(
-				eq(
-					propertyId ? documents.propertyId : documents.id,
-					(propertyId ?? documentId)!
-				)
-			);
+		
+		// Query by document ID
+		if (documentId) {
+			return await pool
+				.select()
+				.from(documents)
+				.where(eq(documents.id, documentId));
+		}
+		
+		// Query by array of property IDs
+		if (Array.isArray(propertyId)) {
+			return await pool
+				.select()
+				.from(documents)
+				.where(inArray(documents.propertyId, propertyId));
+		}
+		
+		// Query by single property ID (propertyId must be a string here)
+		if (propertyId) {
+			return await pool
+				.select()
+				.from(documents)
+				.where(eq(documents.propertyId, propertyId));
+		}
+		
+		// This should never happen due to the discriminated union, but TypeScript needs it
+		return [];
 	},
 	async documentsPath(ids: string[], client: PoolClient | undefined) {
 		const pool = dbConnection();
