75a487c442d2a4d8ba84f667fa4385403b3f40d0
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-01-27 09:07:16 -0500

    PM-00[None][Bug]: fixed commit linting spelling mistake

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,13 +3,13 @@ export default {
   parserPreset: {
     parserOpts: {
       // Add the 'i' flag for case-insensitive match
-      headerPattern: /^([A-Z]+-\d+)\[([^\]]+)\]\[(task|bus)\]: (.+)$/i,
+      headerPattern: /^([A-Z]+-\d+)\[([^\]]+)\]\[(task|bug)\]: (.+)$/i,
       headerCorrespondence: ['ticket', 'scope', 'type', 'subject'],
     },
   },
   rules: {
     // allow both lower and upper forms so the built-in enum check doesn't fail
-    'type-enum': [2, 'always', ['task', 'Task', 'bus', 'Bus']],
+    'type-enum': [2, 'always', ['task', 'Task', 'bug', 'Bug']],
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'header-max-length': [2, 'always', 400],
