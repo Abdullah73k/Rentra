42b4a7fe162104f75542e668b4cc03be377d87db
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-01-28 22:16:24 -0500

    PM-135[PropertyAPI][Task]: updated prod domain as we purchased a new one

--- server/src/constants/domain.constants.ts
+++ server/src/constants/domain.constants.ts
@@ -1,4 +1,4 @@
 export const PRODUCTION_DOMAIN =
-	process.env.PRODUCTION_DOMAIN || "https://property-management-beige.vercel.app";
+	process.env.PRODUCTION_DOMAIN || "https://rentra.tech";
 export const DEVELOPMENT_DOMAIN =
 	process.env.DEVELOPMENT_DOMAIN || "http://localhost:5173";
